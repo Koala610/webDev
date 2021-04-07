5278af2457dc316658cb886ce69c54e39a29887f
Vladlen <vladli2000610@gmail.com>
2021-04-07 14:13:25 +0600

    Added Product and Category models

--- Lab8/shopback/api/models.py
+++ Lab8/shopback/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length = 300)
+    price = models.FloatField(default=0)
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
