15a28ea056d46e13453e3f87fee438be45389598
Vladlen <vladli2000610@gmail.com>
2021-04-07 23:40:26 +0600

    Added normal functionality to the views

--- Lab8/shopback/api/views.py
+++ Lab8/shopback/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, JsonResponse
+from api.models import Product, Category
 
 # Create your views here.
 
@@ -8,17 +9,25 @@ def func(request, id = 0):
 
 
 def product_list(request):
-    dict = {}
-    return JsonResponse(dict,safe=False)
+    products = Product.objects.all()
+    products_json = [product.to_json() for product in products]
+    return JsonResponse(products_json,safe=False)
 
 def product_details(request, id):
-    dict = {"id":id}
-    return JsonResponse(dict,safe=False)
+    products = Product.objects.all()
+    for product in products:
+        if product['id'] == id:
+            return JsonResponse(product.to_json())
+    return JsonResponse({'message': 'Not found'},safe=False)
 
 def category_list(request):
-    dict = {}
-    return JsonResponse(dict,safe=False)
+    categories = Category.objects.all()
+    categories_json = [category.to_json() for category in categories]
+    return JsonResponse(categories_json, safe=False)
 
 def category_details(request,id):
-    dict = {"id":id}
-    return JsonResponse(dict,safe=False)
+    categories = Category.objects.all()
+    for category in categories:
+        if category['id'] == id:
+            return JsonResponse(category.to_json())
+    return JsonResponse({'message': 'Not found'},safe=False)
