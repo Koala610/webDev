2a8534bc59dc62a0420b1109bd4ff6b08edeebc1
Vladlen <vladli2000610@gmail.com>
2021-04-07 14:22:01 +0600

    Created covers of views

--- Lab8/shopback/api/urls.py
+++ Lab8/shopback/api/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path,include
-from api.views import func
+from api.views import func, product_list, product_details,category_list,category_details
+
 
 urlpatterns = [
     path('test/',func),
-    path('products/',func),
-    path('categories/',func),
-    path('products/<int:id>/',func),
-    path('categories/<int:id>/',func)
+    path('products/',product_list),
+    path('categories/',category_list),
+    path('products/<int:id>/',product_details),
+    path('categories/<int:id>/',category_details)
 ]
--- Lab8/shopback/api/views.py
+++ Lab8/shopback/api/views.py
@@ -5,3 +5,20 @@ from django.http.response import HttpResponse, JsonResponse
 
 def func(request, id = 0):
     return HttpResponse('Test' + str(id))
+
+
+def product_list(request):
+    dict = {}
+    return JsonResponse(dict,safe=False)
+
+def product_details(request, id):
+    dict = {"id":id}
+    return JsonResponse(dict,safe=False)
+
+def category_list(request):
+    dict = {}
+    return JsonResponse(dict,safe=False)
+
+def category_details(request,id):
+    dict = {"id":id}
+    return JsonResponse(dict,safe=False)
